1cd86ef01c7971a24277d2cccab7bb2159fc7cc7
ryanburr <ryan.burr2@gmail.com>
2022-08-22 21:51:02 -0400

    chore: use theme in components

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { MjmlButton } from "mjml-react";
+import { theme } from "../styles/theme";
 
 export interface ButtonProps extends React.PropsWithChildren {
   href: string;
 }
 
 export const Button = ({ children, href }: ButtonProps) => {
-  return <MjmlButton href={href}>{children}</MjmlButton>;
+  return (
+    <MjmlButton
+      href={href}
+      backgroundColor={theme.palette.secondary}
+      color={theme.palette.common.white}
+    >
+      {children}
+    </MjmlButton>
+  );
 };
---
+++ styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  palette: {
+    primary: "#001B38",
+    secondary: "#5E7AFF",
+    common: {
+      white: "#F9FAFF",
+      black: "#001B38",
+    },
+  },
+};
